dbbdaecd294183676286e06ba86202c7b2e28049
junkiesdevstudio <junkiesdevstudio@gmail.com>
2015-07-29 18:31:44 +0700

    Add Class cds_BaseKonektor

--- MYDENTIST/MYDENTIST/Class/cds_BaseKonektor.cs
+++ MYDENTIST/MYDENTIST/Class/cds_BaseKonektor.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Data;
 
 namespace MYDENTIST.Class
 {
-    public class cds_MYSQLKonektor : cds_BaseKonektor, IDisposable
+    public abstract class cds_BaseKonektor
     {
-        
+
     }
 }
