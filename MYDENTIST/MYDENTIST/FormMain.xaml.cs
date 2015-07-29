354cfe7c85e69c5ef17c0e2f165f36d6613ab979
junkiesdevstudio <junkiesdevstudio@gmail.com>
2015-07-29 17:37:20 +0700

    Add Icon Image

--- MYDENTIST/MYDENTIST/FormMain.xaml.cs
+++ MYDENTIST/MYDENTIST/FormMain.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MYDENTIST
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FormMain.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FormMain : Window
     {
-        public MainWindow()
+        public FormMain()
         {
             InitializeComponent();
         }
