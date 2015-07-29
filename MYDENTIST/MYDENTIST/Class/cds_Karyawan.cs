43659a65ac93f1bde13a2d6ab40d8c4037de1788
junkiesdevstudio <junkiesdevstudio@gmail.com>
2015-07-29 17:47:40 +0700

    Add Class cds_Karyawan

---
+++ MYDENTIST/MYDENTIST/Class/cds_Karyawan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYDENTIST.Class
+{
+    public class cds_Karyawan
+    {
+        public int IdKaryawan { get; set; }
+        public string NamaKaryawan { get; set; }
+        public string JenisKaryawan { get; set; }
+        public string AlamatKaryawan { get; set; }
+        public string TelpKaryawan { get; set; }
+        public string TglMasukKaryawan { get; set; }
+        public string KeteranganKaryawan { get; set; }
+    }
+}
